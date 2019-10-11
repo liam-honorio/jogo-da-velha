26144f2e92279e54b0745566ba07feaed567f373
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-11 11:35:01 -0300

    Criado o botão sobre

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="menu-header">Sobre</p>
-      </header>
-    </div>
+    <a href="#to-do" className="action">Sobre</a>
   );
 }
 
