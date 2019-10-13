d81a9a27ddd16c59b3405b96c8a79aa72b6053c4
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-13 00:14:05 -0300

    Criou component Header sem css

---
+++ src/objects/Logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CollabCode from '../../img/logo.png';
+
+const Logo = () => {
+ return (<img src={CollabCode} alt="logo da collabcode" />)
+};
+
+export default Logo;
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+
+const Menu = () => (<a href="#to-do" className="menu"><span className="center">Menu</span></a>);
+
+export default Menu;
