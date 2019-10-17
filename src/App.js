d6ed20cf62e86e5625bf0ad96579ae61441604b8
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-17 11:46:27 -0300

    criado o X do fechar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Checkbox from './objects/Checkbox';
 
 import About from './objects/About';
 import AboutLink from './objects/AboutLink';
+import IconClose from './objects/IconClose';
 
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
@@ -20,7 +21,8 @@ const App = () => (
       <Checkbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
       
       <About>
-        <AboutLink />
+        <AboutLink className="-light" />
+        <IconClose /> 
       </About>
       
     </main>
