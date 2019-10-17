35f11cdfa702e5b35b2d16ad35b72e9a9b5af558
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-17 09:53:38 -0300

    Criado o BG do About

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css'
 import Checkbox from './objects/Checkbox';
+import About from './objects/About';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+
 
 
 
@@ -12,6 +14,8 @@ const App = () => (
       <Header />
       <Hashtag />
       <Checkbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
+      
+      <About />
       
     </main>
   );
---
+++ src/objects/About/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css'
+
+const About = () => (
+  <article className="about"></article>
+);
+
+export default About;
