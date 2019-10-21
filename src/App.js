ca83aec5475e54d550ad31ff712d52d1d7e919d8
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-21 17:02:15 -0300

    criado o ProfileUser

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css'
-import Profile from './img/profile.png'
 
 
 import Checkbox from './objects/Checkbox';
 import About from './objects/About';
-import AvatarProfile from './objects/AvatarProfile';
 
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
 import HeaderInternal from './components/HeaderInternal';
+import ProfileUser from './components/ProfileUser';
 
 
 
@@ -27,7 +26,7 @@ const App = () => (
       <About>
         <HeaderInternal />
 
-        <AvatarProfile src={Profile} alt="Avatar do William" />
+        <ProfileUser />
       </About>
       
     </main>
---
+++ src/components/ProfileUser/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css'
+import Profile from '../../img/profile.jpg'
+import AvatarProfile from '../../objects/AvatarProfile';
+
+
+const ProfileUser = () => (
+  <dl className="profile-user">
+    <dd className="avatar">
+      <AvatarProfile src={Profile} alt="Avatar do William" />
+    </dd>
+    <dt className="titulo">William Honorio</dt>
+    <dd className="description">Migrei de palhaço para Dev. Front-End/UX e agora eu trabalho como streamer de código, instrutor e dev na CollabCode</dd>
+  </dl>
+);
+
+export default ProfileUser;
