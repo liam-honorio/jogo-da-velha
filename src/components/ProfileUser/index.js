f7b67bc3901ef7689246c7fecdffb8586724bbf9
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-21 17:36:55 -0300

    Criado e finalizado o ProfileUser de uma forma feliz

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './styles.css'
 import Profile from '../../img/profile.jpg'
+import GitHub from '../../img/github.png'
+import Linkedin from '../../img/linkedin.png'
+import Facebook from '../../img/facebook.png'
+import Twitter from '../../img/twitter.png'
+import Instagram from '../../img/instagram.png'
+
+
 import AvatarProfile from '../../objects/AvatarProfile';
+import IconSocial from '../../objects/IconSocial';
 
 
 const ProfileUser = () => (
@@ -11,6 +19,13 @@ const ProfileUser = () => (
     </dd>
     <dt className="titulo">William Honorio</dt>
     <dd className="description">Migrei de palhaço para Dev. Front-End/UX e agora eu trabalho como streamer de código, instrutor e dev na CollabCode</dd>
+    <dd className="socials">
+      <IconSocial action="https://github.com/liam-honorio/" src={GitHub} alt="Logo do GitHub" />
+      <IconSocial action="https://www.linkedin.com/in/william-honorio-5b7984124/" src={Linkedin} alt="Logo do Linkedin" />
+      <IconSocial action="https://www.facebook.com/liam.honorio" src={Facebook} alt="Logo do Facebook" />
+      <IconSocial action="https://twitter.com/lost_box" src={Twitter} alt="Logo do Twitter" />
+      <IconSocial action="https://www.instagram.com/lost_box/" src={Instagram} alt="Logo do Instagram" />
+    </dd>
   </dl>
 );
 
---
+++ src/objects/IconSocial/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconSocial = ({ action, src, alt }) => (
+  <a target="_blank" href={action}>
+    <img className="icon-social" src={src} alt={alt} />
+  </a>
+);
+
+export default IconSocial;
