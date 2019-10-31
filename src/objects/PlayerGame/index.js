e5429dbb7e0b5894f2663334073594b7da8e2086
William Honorio <mhf.eng.dsg@gmail.com>
2019-10-31 11:03:49 -0300

    agora o playergame pode ficar sem valor no player

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,13 +3,15 @@ import './styles.css';
 import playerX from '../../img/player-x.png'
 import playerO from '../../img/player-o.png'
 
-const PlayerGame = ({player}) => {
+const PlayerGame = ({player = false}) => {
   const players = [];
   players['x'] = playerX;
   players['o'] = playerO
-  return <button className="player-game">
-    <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>
-  </button>
+  return (
+    <button className="player-game">
+      {player && <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>}
+    </button>
+  )
 };
 
 export default PlayerGame;
