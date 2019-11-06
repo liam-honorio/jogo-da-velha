131a26d92a71aeca630be6427270e51b6e4dce28
William Honorio <mhf.eng.dsg@gmail.com>
2019-11-06 18:32:09 -0200

    responsivo do history game

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css'
 
 import InputCheckbox from './objects/InputCheckbox';
 import LayerDark from './objects/LayerDark';
+import WrapperHashtagHistory from './objects/WrapperHashtagHistory';
 
 import HeaderGame from './components/HeaderGame';
 import HashtagGame from './components/HashtagGame';
 import HeaderInternal from './components/HeaderInternal';
 import ProfileUser from './components/ProfileUser';
 import HistoryGame from './components/HistoryGame';
+
 
 
 
@@ -34,10 +36,11 @@ const App = () => {
   return(
     <main id="main" className="app">
       <HeaderGame onClick={handleClickAdd} />
-      <HashtagGame callback={addHistory} />
-      <InputCheckbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
-      
-      <HistoryGame history={ history } />
+      <WrapperHashtagHistory>
+        <HashtagGame callback={addHistory} />
+        <InputCheckbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
+        <HistoryGame history={ history } />
+      </WrapperHashtagHistory>
 
       <LayerDark className={activeAbout}>
         <HeaderInternal onClick={handleClickRemove}/>
---
+++ src/objects/WrapperHashtagHistory/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WrapperHashtagHistory = ({children}) => (<div className="wrapper-hashtagHistory">
+  {children}
+</div>);
+
+export default WrapperHashtagHistory;
