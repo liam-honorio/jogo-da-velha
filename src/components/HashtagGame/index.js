4c96c45181b128e3cf6cc55fc2a55c1c986a292b
William Honorio <mhf.eng.dsg@gmail.com>
2019-12-08 17:44:50 -0200

    Mostra ultimo estado do hashtagGame

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -5,7 +5,7 @@ import PlayerGame from "../../objects/PlayerGame";
 
 const HashtagGame = ({ callback }) => {
   const [nextPlayer, setNextPlayer] = useState("x");
-  const [round, setRound] = useState(0);
+  const [lastRound, setRound] = useState(0);
   const [history, setHistory] = useState([
     {
       round: 0,
@@ -22,31 +22,20 @@ const HashtagGame = ({ callback }) => {
       ]
     }
   ]);
-  const [players, setPlayers] = useState([
-    { id: 1, content: "" },
-    { id: 2, content: "" },
-    { id: 3, content: "" },
-    { id: 4, content: "" },
-    { id: 5, content: "" },
-    { id: 6, content: "" },
-    { id: 7, content: "" },
-    { id: 8, content: "" },
-    { id: 9, content: "" }
-  ]);
 
   const handleClick = id => {
-    setPlayers(old =>
-      old.map(player =>
-        player.id === id ? { id, content: nextPlayer } : player
-      )
-    );
+    // setPlayers(old =>
+    //   old.map(player =>
+    //     player.id === id ? { id, content: nextPlayer } : player
+    //   )
+    // );
     callback(nextPlayer);
     setNextPlayer(old => (old === "x" ? "o" : "x"));
   };
   return (
     <CardGame>
       <ul className="hashtag-game">
-        {players.map(({ id, content }) => (
+        {history[lastRound].state.map(({ id, content }) => (
           <li
             key={id}
             className="item"
