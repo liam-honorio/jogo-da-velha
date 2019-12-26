5bfe4d8a713688f3b0775288f107065cac3871ab
William Honorio <mhf.eng.dsg@gmail.com>
2019-12-26 10:53:56 -0200

    Maquina do Tempo

--- src/objects/WrapperHashtagHistory/index.js
+++ src/objects/WrapperHashtagHistory/index.js
@@ -7,6 +7,24 @@ import HashtagGame from "../../components/HashtagGame";
 const WrapperHashtagHistory = () => {
   const [history, setHistory] = useState(["Start"]);
   const [active, setActive] = useState(false);
+  const [nextPlayer, setNextPlayer] = useState("x");
+  const [lastRound, setLastRound] = useState(0);
+  const [historyGame, setHistoryGame] = useState([
+    {
+      round: 0,
+      state: [
+        { id: 1, content: "" },
+        { id: 2, content: "" },
+        { id: 3, content: "" },
+        { id: 4, content: "" },
+        { id: 5, content: "" },
+        { id: 6, content: "" },
+        { id: 7, content: "" },
+        { id: 8, content: "" },
+        { id: 9, content: "" }
+      ]
+    }
+  ]);
   const addHistory = player =>
     setHistory(old => [...old, `Adicionou ${player.toUpperCase()}`]);
   const showHideHistory = () => {
@@ -14,11 +32,32 @@ const WrapperHashtagHistory = () => {
   };
   const changeHistory = key => {
     setHistory(old => old.slice(0, key + 1));
+    setLastRound(key);
+    setHistoryGame(old => old.slice(0, key + 1));
+  };
+
+  const handleClick = id => {
+    setHistoryGame(old => [
+      ...old,
+      {
+        round: lastRound + 1,
+        state: old[lastRound].state.map(player =>
+          player.id === id ? { id, content: nextPlayer } : player
+        )
+      }
+    ]);
+    setLastRound(old => old + 1);
+    addHistory(nextPlayer);
+    setNextPlayer(old => (old === "x" ? "o" : "x"));
   };
 
   return (
     <div className={`wrapper-hashtagHistory ${active && "-active"}`}>
-      <HashtagGame callback={addHistory} />
+      <HashtagGame
+        lastRound={lastRound}
+        historyGame={historyGame}
+        onClick={handleClick}
+      />
 
       <InputCheckbox
         onClick={showHideHistory}
